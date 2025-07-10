48c16fb75a830e90db36f89beaec214349f8557f
Shabeebmohmd <shabeebmohmd49@gmail.com>
2025-07-10 11:29:43 +0530

    feature: change carousel into helper method

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -28,92 +28,93 @@ class HomeScreen extends StatelessWidget {
                 _buildSearch(context),
                 Utils.kSpacingH,
                 _buildCategory(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 10,
-                  children: [
-                    GoogleFontText(
-                      'Top picks for you',
-                      fontFamily: 'Quicksand',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                    ),
-                    CarouselSlider.builder(
-                      itemCount: 2,
-                      itemBuilder: (context, index, _) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Container(
-                            // margin: EdgeInsets.symmetric(horizontal: 10),
-                            decoration: BoxDecoration(
-                              color: AppColors.primary,
-                              borderRadius: BorderRadius.circular(5),
-                              // image: DecorationImage(
-                              //   image: AssetImage('assets/images/fruits.png'),
-                              // ),
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    top: 40,
-                                    left: 20,
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      GoogleFontText(
-                                        'DISCOUNT\n25% ALL\nFRUITS',
-                                        fontFamily: 'Poppins',
-                                        fontWeight: FontWeight.w700,
-                                        fontSize: 20,
-                                        color: AppColors.backGround,
-                                      ),
-                                      Utils.kSpacingH,
-                                      MaterialButton(
-                                        onPressed: () {},
-                                        color: AppColors.button_1,
-                                        child: GoogleFontText(
-                                          'CHECK NOW',
-                                          fontFamily: 'Poppins',
-                                          color: AppColors.backGround,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 4,
-                                  child: Image.asset(
-                                    'assets/images/fruits.png',
-                                    width: 205,
-                                    height: 197,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      options: CarouselOptions(
-                        height: 200,
-                        reverse: false,
-                        autoPlay: false,
-                        autoPlayInterval: const Duration(seconds: 3),
-                        scrollDirection: Axis.horizontal,
-                        initialPage: 0,
-                        viewportFraction: 1.0,
-                        padEnds: true,
-                      ),
-                    ),
-                  ],
-                ),
+                _buildCarousel(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Column _buildCarousel() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 10,
+      children: [
+        GoogleFontText(
+          'Top picks for you',
+          fontFamily: 'Quicksand',
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+        CarouselSlider.builder(
+          itemCount: 2,
+          itemBuilder: (context, index, _) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Container(
+                // margin: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                  // image: DecorationImage(
+                  //   image: AssetImage('assets/images/fruits.png'),
+                  // ),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40, left: 20),
+                      child: Column(
+                        children: [
+                          GoogleFontText(
+                            'DISCOUNT\n25% ALL\nFRUITS',
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                            color: AppColors.backGround,
+                          ),
+                          Utils.kSpacingH,
+                          MaterialButton(
+                            onPressed: () {},
+                            color: AppColors.button_1,
+                            child: GoogleFontText(
+                              'CHECK NOW',
+                              fontFamily: 'Poppins',
+                              color: AppColors.backGround,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Image.asset(
+                        'assets/images/fruits.png',
+                        width: 205,
+                        height: 197,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          options: CarouselOptions(
+            height: 200,
+            reverse: false,
+            autoPlay: false,
+            autoPlayInterval: const Duration(seconds: 3),
+            scrollDirection: Axis.horizontal,
+            initialPage: 0,
+            viewportFraction: 1.0,
+            padEnds: true,
+          ),
+        ),
+      ],
     );
   }
 
