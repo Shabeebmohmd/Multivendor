97818213e3e72d40e7ba6326367f726d0f5e3201
Shabeebmohmd <shabeebmohmd49@gmail.com>
2025-07-10 10:52:57 +0530

    features: add carousel

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xFF29D177);
-  static const offertagColor = Color.fromARGB(255, 117, 81, 179);
+  static const offertagColor = Color.fromARGB(255, 104, 91, 203);
   static const red = Colors.red;
   static const backGround = Colors.white;
+  static const button_1 = Color.fromARGB(255, 255, 137, 24);
 }
